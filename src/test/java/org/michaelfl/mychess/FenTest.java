b43d2f7aceceb27a4a5ed13e419500ba035a230c
Michael Fleischhauer <mf@censhare.com>
2020-11-11 12:21:51 +0100

    fix: fix failing FEN test

--- src/test/java/org/michaelfl/mychess/FenTest.java
+++ src/test/java/org/michaelfl/mychess/FenTest.java
@@ -32,7 +32,7 @@ class FenTest {
         SimpleNotationImporter importer = new SimpleNotationImporter("[[e2-e4 e7-e5 g1-f3 b8-c6 f1-b5 a7-a6 b5-a4 b7-b5 a4-b3 g8-f6 e1-g1 a6-a5 d2-d4 a5-a4 b3-f7 e8-f7 d4-e5 f6-g8 f3-g5 f7-e8 b1-c3 b5-b4 d1-d5 g8-h6 c3-b5 a4-a3 f1-d1 c8-b7]]");
         var game = importer.importGame();
 
-        String expectedFEN = "r2qkb1r/1bpp2pp/2n4n/1N1QP1N1/1p2P3/p7/PPP2PPP/R1BR2K1 w - - 0 15";
+        String expectedFEN = "r2qkb1r/1bpp2pp/2n4n/1N1QP1N1/1p2P3/p7/PPP2PPP/R1BR2K1 w - - 2 15";
         assertEquals(expectedFEN, game.exportFEN(), "wrong FEN");
     }
 }
